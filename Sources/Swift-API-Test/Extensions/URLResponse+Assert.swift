f3db42d8b18f94cea38e630fc1e789e9ab3390bb
tanaka <lemo.nade.engineer.room@gmail.com>
2022-08-06 19:43:56 +0900

    ステータスコードの比較をXCTAssertEqualで行うよう変更

--- Sources/Swift-API-Test/Extensions/URLResponse+Assert.swift
+++ Sources/Swift-API-Test/Extensions/URLResponse+Assert.swift
@@ -4,14 +4,10 @@ import XCTest
 extension URLResponse {
     
     func assert(_ status: HTTPStatus) {
-        if isStatus(status) { return }
-        XCTFail()
-    }
-    
-    private func isStatus(_ status: HTTPStatus) -> Bool {
-        if let actual = (self as? HTTPURLResponse)?.statusCode {
-            return actual == Int(status.code)
+        guard let actual = (self as? HTTPURLResponse)?.statusCode else {
+            XCTFail("response has not status code")
+            return
         }
-        return false
+        XCTAssertEqual(Int(status.code), actual)
     }
 }
